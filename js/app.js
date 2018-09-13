083c87d7267f8ef0ac617dea5ece795fcf7fd5f9
LehmanBrother <chrislehman1991@gmail.com>
2018-09-12 21:08:50 -0500

    Commit 9 - Project Euler Problem 1 answered

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ for(i = 0; i < ninjaTurtles.length; i++) {
 }*/
 
 //Return of the Closets
-const kristynsCloset = [
+/*const kristynsCloset = [
   "left shoe",
   "cowboy boots",
   "right sock",
@@ -126,11 +126,17 @@ for(i = 0; i < kristynsCloset.length; i++) {
 //Inventory
 for(i = 0; i < thomsCloset.length; i++) {
 	console.log(thomsCloset[i]);
+}*/
+
+//Multiples of 3 and 5
+let sum = 0;
+for(i = 1; i < 1000; i++) {
+	if(i % 3 === 0 || i % 5 === 0) {
+		sum += i;
+	}
 }
 
-
-
-
+console.log(sum);
 
 
 
